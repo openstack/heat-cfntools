3fe50d72e1331018909cca2716d927937a6ca463
Steven Dake <sdake@redhat.com>
2012-06-05 16:07:21 -0700

    add setup.py to heat-jeos command
    Signed-off-by: Steven Dake <sdake@redhat.com>

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import gettext
+import os
+import subprocess
+
+import setuptools
+
+setuptools.setup(
+    name='heat_jeos',
+    version='1',
+    description='The heat-jeos project provides services for creating '
+                '(J)ust (E)nough (O)perating (S)ystem images',
+    license='Apache License (2.0)',
+    author='Heat API Developers',
+    author_email='discuss@heat-api.org',
+    url='http://heat-api.org.org/',
+    packages=setuptools.find_packages(exclude=['bin']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'Environment :: No Input/Output (Daemon)',
+    ],
+    scripts=['bin/heat-jeos'],
+    py_modules=[])
