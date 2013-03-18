1cbd58fc00974033fad96c8da2adf3959fd8a1ea
Steve Baker <sbaker@redhat.com>
2013-03-19 10:56:38 +1300

    Prep for release 1.2.1
    Change-Id: I38d97815e32cfacaa8a46e255d3f97c459295ce1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(fname):
 
 setuptools.setup(
     name='heat-cfntools',
-    version='1.2',
+    version='1.2.1',
     description='Tools required to be installed on Heat '
     'provisioned cloud instances',
     long_description=read('README.rst'),
