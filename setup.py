206650597bc9918691f5ed0ac63d8318db23d899
Monty Taylor <mordred@inaugust.com>
2013-05-11 13:49:12 -0400

    Migrate to pbr.
    Fixes bug 1179007
    
    Change-Id: Iaa8a2722938d0b25df1041695f3c1c8dbf563b47

--- setup.py
+++ setup.py
@@ -1,50 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
-# Copyright (c) 2013 Red Hat, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import os
 import setuptools
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setuptools.setup(
-    name='heat-cfntools',
-    version='1.2.3',
-    description='Tools required to be installed on Heat '
-    'provisioned cloud instances',
-    long_description=read('README.rst'),
-    license='Apache License (2.0)',
-    author='Heat API Developers',
-    author_email='discuss@heat-api.org',
-    url='http://heat-api.org.org/',
-    packages=setuptools.find_packages(exclude=['bin']),
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.6',
-        'Environment :: No Input/Output (Daemon)',
-    ],
-    scripts=[
-        'bin/cfn-get-metadata',
-        'bin/cfn-hup',
-        'bin/cfn-init',
-        'bin/cfn-push-stats',
-        'bin/cfn-signal',
-        'bin/cfn-create-aws-symlinks'],
-    py_modules=[])
+    setup_requires=['d2to1>=0.2.10,<0.3', 'pbr>=0.5.10,<0.6'],
+    d2to1=True)
